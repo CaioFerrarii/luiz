6c98dfa1ed9fa4de9e1e0ae12b97c958e8ea5e02
Aluno.Etec <aula@etec.com>
2025-11-14 17:15:01 -0300

    inicial

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
